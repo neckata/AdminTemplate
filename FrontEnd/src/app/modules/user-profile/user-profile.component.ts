83a1e0342793ef5a43012caa75e9820a63466592
Nikola Netsov <pikedbs@abv.bg>
2021-06-02 00:53:15 +0300

    Resources

--- FrontEnd/src/app/modules/user-profile/user-profile.component.ts
+++ FrontEnd/src/app/modules/user-profile/user-profile.component.ts
@@ -1,9 +1,17 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
-  selector: 'app-user-profile',
-  templateUrl: './user-profile.component.html',
-  styleUrls: ['./user-profile.component.css']
+    selector: 'app-user-profile',
+    templateUrl: './user-profile.component.html',
+    styleUrls: ['./user-profile.component.css']
 })
-export class UserProfileComponent {
+export class UserProfileComponent implements OnInit {
+
+    ngOnInit(): void {
+        throw new Error('Method not implemented.');
+    }
+
+    updateProfile(): void {
+        throw new Error('Method not implemented.');
+    }
 }
